943a306ceb034ba87dd3666780650d9f240ee500
GiorgiAkhalkatsishvili <146560992+GiorgiAkhalkatsishvili@users.noreply.github.com>
2026-03-13 20:19:18 +0400

    .

--- kidsweb/src/App.js
+++ kidsweb/src/App.js
@@ -1,17 +1,30 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import PuzzleGame from './pages/PuzzleGamePage';
 import AuthPage from './pages/AuthPage';
+import HeaderComponent from './components/HeaderComponent';
 
-function App() {
+function AppWrapper() {
+  const location = useLocation();
+  const showHeader = location.pathname !== '/auth'; // hide on /auth only
+
   return (
-    <BrowserRouter>
+    <>
+      {showHeader && <HeaderComponent />}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/puzzle" element={<PuzzleGame />} />
         <Route path="/auth" element={<AuthPage />} />
       </Routes>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppWrapper />
     </BrowserRouter>
   );
 }
--- kidsweb/src/pages/HomePage/index.jsx
+++ kidsweb/src/pages/HomePage/index.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import HeaderComponent from "../../components/HeaderComponent";
 import HeroComponent from "../../components/HeroComponent";
 import SubjectsComponent from "../../components/SubjectsComponent";
 import DailyChallengeComponent from "../../components/DailyChallengeComponent";
@@ -8,7 +7,6 @@ import DailyChallengeComponent from "../../components/DailyChallengeComponent";
 function HomePage() {
   return (
     <div className="HomePage">
-      <HeaderComponent />
       <HeroComponent />
       <SubjectsComponent />
       <DailyChallengeComponent />
