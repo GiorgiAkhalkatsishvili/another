ba174620895ab4b0d9590745a716991dc78e4023
GiorgiAkhalkatsishvili <146560992+GiorgiAkhalkatsishvili@users.noreply.github.com>
2026-03-13 20:25:54 +0400

    .

--- kidsweb/src/App.js
+++ kidsweb/src/App.js
@@ -4,19 +4,23 @@ import HomePage from './pages/HomePage';
 import PuzzleGame from './pages/PuzzleGamePage';
 import AuthPage from './pages/AuthPage';
 import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
 
 function AppWrapper() {
   const location = useLocation();
-  const showHeader = location.pathname !== '/auth'; // hide on /auth only
+  const showHeaderAndFooter = location.pathname !== '/auth'; // hide on /auth only
 
   return (
     <>
-      {showHeader && <HeaderComponent />}
+      {showHeaderAndFooter && <HeaderComponent />}
+      
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/puzzle" element={<PuzzleGame />} />
         <Route path="/auth" element={<AuthPage />} />
       </Routes>
+
+      {showHeaderAndFooter && <FooterComponent />}
     </>
   );
 }
